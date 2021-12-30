4d23da3e556e3eb92254038cc88a8c8cd33ef87b
Austin Ruby <austinjruby@gmail.com>
2021-12-30 11:03:43 -0500

    feat: add underline to links

--- src/Descriptor.svelte
+++ src/Descriptor.svelte
@@ -6,37 +6,69 @@
     export let link;
 
     const fadeConfig = { duration: 700 };
+
+    const textColor = '#E0CEF3';
+    const textColorHover = '#C19CE7'
 </script>
 
 {#if visible}
     {#if link}
-        <a href={link} target='none' in:fade={fadeConfig}>{text}</a>
+        <a
+            href={link}
+            target='none'
+            in:fade={fadeConfig}
+            style='--text-color: {textColor}; --text-color-hover: {textColorHover};'
+        >
+            {text}
+        </a>
     {:else}
-        <p in:fade={fadeConfig}>{text}</p>
+        <p style='--text-color: {textColor};' in:fade={fadeConfig}>{text}</p>
     {/if}
 {/if}
 
 <style>
     p, a {
-		color: #E0CEF3;
+		color: var(--text-color);
 		font-size: 2vw;
 		margin: 0;
         font-family: 'Nunito Sans', sans-serif;
-        text-decoration: none;
-        transition: transform .2s, color .2s ease;
         padding: 0 2rem;
 	}
 
+    a {
+        position: relative;
+        text-decoration: none;
+        transition: transform .2s, color .2s ease;
+    }
+
+    a::before {
+        content: "";
+        position: absolute;
+        display: block;
+        width: calc(100% - 4.5rem);
+        height: 2px;
+        bottom: 0;
+        left: 2.25rem;
+        background-color: var(--text-color);
+        transform: scaleX(0);
+        transition: transform 0.2s ease;
+    }
+
     @media(hover: hover) {
         a:hover {
-            color: #C19CE7;
-            transform: scale(1.25);
+            color: var(--text-color-hover);
+            transform: scale(1.1);
+        }
+
+        a:hover::before {
+            background-color: var(--text-color-hover);
+            transform: scaleX(1);
         }
     }
 
     @media(max-width: 768px) {
         p {
-            font-size: 4.5vw;
+            font-size: 16vw;
         }
     }
 </style>
