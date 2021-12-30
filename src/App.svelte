c822fb29e0f5152721eec82f2b270ca2f4e8f9ea
Austin Ruby <austinjruby@gmail.com>
2021-12-29 22:03:30 -0500

    feat: add links and hover effects to descriptors

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,16 @@
 
 	let activeIndex = 0;
 
-	// TODO add links to some of these
-	// TODO add clickme tooltip
 	const descriptors = [
-		'Software engineer @ Medly',
-		'Scrambling golfer',
-		'Ex-bartender',
-		'Opinionated bagel lover',
-		'Used book reader',
-		'Ex-Tesla',
-		'Dog parent',
-		'Soul revival listener',
-		'Ex-pastrami hawker',
+		{ text: 'Software engineer @ Medly', link: 'https://medly.com/en-us' },
+		{ text: 'Delusional golfer', link: 'https://www.youtube.com/watch?v=zbQTXFJL8lo&ab_channel=MiGustaTacoBell' },
+		{ text: 'Ex-bartender', link: 'https://www.instagram.com/2ndfloorbaressen/' },
+		{ text: 'Opinionated bagel lover', link: 'http://ourbksocial.com/searching-brooklyns-bodacious-bagel-conclusion/' },
+		{ text: 'Used book reader', link: 'https://www.thriftbooks.com/w/post-office_charles-bukowski/265076/?resultid=37acd627-02cd-4745-96de-93ef48d06427#edition=14624442&idiq=46472774' },
+		{ text: 'Ex-Tesla', link: 'https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor' },
+		{ text: 'Dog parent', link: '/cora.jpg' },
+		{ text: 'Soul revival listener', link: 'https://open.spotify.com/track/7APxY3crOzJpHfIwH57DcH?si=b34dfec57e164258' },
+		{ text: 'Ex-pastrami hawker', link: 'https://2ndavedeli.com/' },
 	];
 
 	onMount(() => {
@@ -35,8 +33,8 @@
 <main>
 	<div id="header-container">
 		<h1>Austin Ruby</h1>
-		{#each descriptors as descriptor, index}
-			<Descriptor text={descriptor} visible={index === activeIndex} />
+		{#each descriptors as { text, link }, index}
+			<Descriptor {text} {link} visible={index === activeIndex} />
 		{/each}
 	</div>
 </main>
--- src/Descriptor.svelte
+++ src/Descriptor.svelte
@@ -3,19 +3,36 @@
 
     export let text;
     export let visible;
+    export let link;
+
+    const fadeConfig = { duration: 700 };
 </script>
 
 {#if visible}
-    <p in:fade={{ duration: 700 }}>{text}</p>
+    {#if link}
+        <a href={link} target='none' in:fade={fadeConfig}>{text}</a>
+    {:else}
+        <p in:fade={fadeConfig}>{text}</p>
+    {/if}
 {/if}
 
 <style>
-    p {
+    p, a {
 		color: #E0CEF3;
 		font-size: 2vw;
 		margin: 0;
         font-family: 'Nunito Sans', sans-serif;
+        text-decoration: none;
+        transition: transform .2s, color .2s ease;
+        padding: 0 2rem;
 	}
+
+    @media(hover: hover) {
+        a:hover {
+            color: #C19CE7;
+            transform: scale(1.25);
+        }
+    }
 
     @media(max-width: 768px) {
         p {
